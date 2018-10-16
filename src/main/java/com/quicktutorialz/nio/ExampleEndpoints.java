18e0644c5d5756ef63d074c11d9b95daaac3186b
Alessandro Argentieri <alexmawashi87@gmail.com>
2018-10-16 13:10:28 +0200

    added Flowable since http request

--- src/main/java/com/quicktutorialz/nio/ExampleEndpoints.java
+++ src/main/java/com/quicktutorialz/nio/ExampleEndpoints.java
@@ -5,9 +5,12 @@ import com.alexmawashi.nio.utils.*;
 import com.quicktutorialz.nio.entities.UserData;
 import com.quicktutorialz.nio.services.ReactiveService;
 import com.quicktutorialz.nio.services.ReactiveServiceImpl;
+import io.reactivex.Flowable;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Observable;
 
 
 //TODO: multipart, form, file upload (nio?) https://stackoverflow.com/questions/2422468/how-to-upload-files-to-server-using-jsp-servlet
@@ -30,6 +33,15 @@ public class ExampleEndpoints extends Endpoints{
     private final Action createUserJson = (HttpServletRequest request, HttpServletResponse response) -> {
         final UserData userData = (UserData) getDataFromJsonBodyRequest(request, UserData.class);
         service.createUserCompletelyNIO(userData)
+                .subscribe(res -> toJsonResponse(request, response, res),
+                            t  -> toJsonResponse(request, response, t)    );
+    };
+
+    @Api(path = "/create/user/json/allreactive", method = "POST", consumes = "application/json", produces = "application/json", description = "")
+    private final Action createUserJsonInputReactive = (HttpServletRequest request, HttpServletResponse response) -> {
+        Flowable.just(request)
+                .map( req -> (UserData) getDataFromJsonBodyRequest(req, UserData.class))
+                .map(userData -> service.createUserCompletelyNIO(userData))
                 .subscribe(res -> toJsonResponse(request, response, res),
                             t  -> toJsonResponse(request, response, t)    );
     };
@@ -87,6 +99,7 @@ public class ExampleEndpoints extends Endpoints{
 
     public ExampleEndpoints(){
         setEndpoint("/create/user/json", createUserJson);
+        setEndpoint("/create/user/json/allreactive", createUserJsonInputReactive);
         setEndpoint("/create/user/text", createUserText);
         setEndpoint("/create/user/xml",  createUserXml);
         setEndpoint("/create/user/xml/plus/header",  createUserXmlPlusHeader);
